28d28864017fd3830d366745badbd8cc616fef1d
abrhamzewdu2119 <abrhamzewdu2119@gmail.com>
2024-07-10 19:41:34 +0300

    first commit

---
+++ app/hello/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div className=' w-full h-10 text-2xl bg-red-700 flex items-center justify-center text-white'>
+              navbar
+    </div>
+  )
+}
+
+export default layout
---
+++ app/hello/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello rich
+    </div>
+  )
+}
+
+export default page
